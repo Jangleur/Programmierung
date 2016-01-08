b3229a0b17f5ef93a2c1fc87f7624904f4b1b6a8
Jangleur <albert@rhrk.uni-kl.de>
2016-01-08 16:25:26 +0100

    Porblem1

--- C++/ProjectEuler/Problem1/problem1.cpp
+++ C++/ProjectEuler/Problem1/problem1.cpp
@@ -12,8 +12,7 @@ int main()
   int sum = 0;
   for (int i = 1; i < 1000; i++)
   {
-    if ((i % 3 == 0) || (i % 5 == 0))
-      sum += i;
+   sum += (i % 3 == 0 || i % 5 == 0) ? i : 0;
   }
   
   cout << sum << endl;
