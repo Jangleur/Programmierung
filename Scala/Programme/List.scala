4298ab535e8d2c57324d7380ffc02ecbc75be342
Jan Albert <jangleur@github.com>
2018-10-19 15:42:42 +0200

    List erweitert

--- Scala/Programme/List.scala
+++ Scala/Programme/List.scala
@@ -1,23 +1,40 @@
-sealed trait List[+A] // `List` data type, parameterized on a type, `A`
-case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
-/* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
-which may be `Nil` or another `Cons`.
- */
-case class Cons[+A](head: A, tail: List[A]) extends List[A]
+trait MyList[+A]
+case object Nil extends MyList[Nothing]
+case class Cons[+A] (head: A, tail: MyList[A]) extends MyList[A]
 
-object List { // `List` companion object. Contains functions for creating and working with lists.
-  def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
-    case Nil => 0 // The sum of the empty list is 0.
-    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+object MyList { 
+  def sum(ints: MyList[Int]): Int = ints match { 
+    case Nil => 0 
+    case Cons(x,xs) => x + sum(xs) 
   }
 
-  def product(ds: List[Double]): Double = ds match {
+  def product(ds: MyList[Double]): Double = ds match {
     case Nil => 1.0
     case Cons(0.0, _) => 0.0
     case Cons(x,xs) => x * product(xs)
   }
 
-  def apply[A](as: A*): List[A] = // Variadic function syntax
+  def drop[A](l: MyList[A], n: Int): MyList[A] = {
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_,t) => drop(t, n-1)
+    }
+  }
+
+  def dropWhile[A](l: MyList[A], p: A => Boolean): MyList[A] = l match {
+     case Cons(h, t) if p(h) => dropWhile(t, p)
+     case _ => l
+  }
+
+  def append[A](a1: MyList[A], a2: MyList[A]): MyList[A] = a1 match {
+    case Nil => a2
+    case Cons(h,t) => Cons(h, append(t, a2))
+  }
+
+  def apply[A](as: A*): MyList[A] = 
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 }
+
+
