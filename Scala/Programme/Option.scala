473da031b38a514440534fdcd0dde60dd1d1d559
Jan Albert <jangleur@github.com>
2018-11-03 16:08:45 +0100

    Variance hinzugefuegt

--- Scala/Programme/Option.scala
+++ Scala/Programme/Option.scala
@@ -1,27 +1,36 @@
-sealed trait Option[+A] {
+sealed trait MyOption[+A] {
+   
+   def map[B](f: A => B): MyOption[B] = this match {
+      case None => None
+      case Some(a) => Some(f(a))
+   }
 
-  def map[B](f: A => B): Option[B] = this match {
-    case None => None
-    case Some(a) => Some(f(a))
-  }
+   def getOrElse[B >: A](default: => B): B = this match {
+      case None => default
+      case Some(a) => a
+   }
 
-  def flatMap[B](f: A => Option[B]): Option[B] =
-    map(f) getOrElse None
-  
-  def getOrElse[B >: A](default: => B): B = this match {
-    case None => default
-    case Some(a) => a
-  } 
-  
-  def orElse[B >: A](obj: => Option[B]): Option[B] =
-    this map (Some(_)) getOrElse obj	
+   def flatMap[B](f: A => MyOption[B]): MyOption[B] =
+      map(f) getOrElse None
 
-  def filter(p: A => Boolean): Option[A] = this match {
-    case Some(a) if p(a) => Some(a)
-    case _ => None
-  }
+   def orElse[B >: A](ob: => MyOption[B]): MyOption[B] =
+      this map(x => Some(x)) getOrElse ob
+
+   def filter(p: A => Boolean): MyOption[A] =
+      flatMap(a => if (p(a)) Some(a) else None)
+
 }
 
-case class Some[+A](get: A) extends Option[A]
-case object None extends Option[Nothing]
+case class Some[+A](get: A) extends MyOption[A]
+case object None extends MyOption[Nothing]
+
+object MyOption {
+
+  def mean(xs: Seq[Double]): MyOption[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): MyOption[Double] =
+     mean(xs) flatMap (m => mean(xs map (x => math.pow(x - m, 2))))
+}
 
